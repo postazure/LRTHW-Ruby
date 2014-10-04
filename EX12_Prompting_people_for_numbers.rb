b4bb6dc4754adcb6b75d6f59decb938881c6e016
postazure <gordonmaxc@gmail.com>
2014-10-04 09:53:08 -0700

    Added EX13

--- EX12_Prompting_people_for_numbers.rb
+++ EX12_Prompting_people_for_numbers.rb
@@ -1,8 +1,9 @@
 print "Give me a number: "
-number = gets.chomp.to_i
+number = gets.chomp.to_i #if you leave off the .to_i here...
 
-bigger = number * 100
-puts "A bigger number is #{bigger}."
+bigger = number * 100 #...number is a string not an integer, string*100....
+puts "A bigger number is #{bigger}." #... just prints the number 100 times.
+                                     # It doesn't print a larger number.
 
 print "Give me another number: "
 another = gets.chomp
